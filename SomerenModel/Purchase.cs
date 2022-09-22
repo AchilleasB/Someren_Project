f1d55e3dd88296311f1755643dd46f979c7f8d0e
AchilleasB <90069683+AchilleasB@users.noreply.github.com>
2022-09-22 21:45:07 +0200

    Someren Model

---
+++ SomerenModel/Purchase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Purchase
+    {
+        public int PurchaseId { get; set; }
+        public int StudentId { get; set; } // LecturerNumber, e.g. 47198
+        public int DrinkId { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+    }
+}
---
+++ SomerenModel/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Digest { get; set; }
+        public string Salt { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool Role { get; set; }
+        public string SecretQuestion { get; set; }
+        public string SecretAnswer { get; set; }
+
+    }
+}
